70f00ec44ed9e7117b11cc3e55c0660feba358e0
David Shi <dshi@yaharasoftware.com>
2018-11-08 20:39:42 -0600

    MaxIncreaseKeepingSkyline

--- LeetCode/FirstTenEasy.cs
+++ LeetCode/FirstTenEasy.cs
@@ -23,5 +23,48 @@ namespace LeetCode
 
             return count;
         }
+
+        [Test]
+        public void MaxIncreaseKeepingSkyline()
+        {
+            var a = new int[4][];
+            a[0] = new[] { 3, 0, 8, 4 };
+            a[1] = new[] { 2, 4, 5, 7 };
+            a[2] = new[] { 9, 2, 6, 3 };
+            a[3] = new[] { 0, 3, 1, 0 };
+            var t = MaxIncreaseKeepingSkyline(a);
+        }
+
+        public int MaxIncreaseKeepingSkyline(int[][] grid)
+        {
+            if (grid?[0] == null)
+            {
+                return 0;
+            }
+
+            var rowMax = new int[grid[0].Length];
+            var colMax = new int[grid.Length];
+
+            for (var i = 0; i < grid.Length; i++)
+            {
+                rowMax[i] = grid[i].Max();
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    colMax[i] = Math.Max(colMax[i], grid[j][i]);
+                }
+            }
+
+            var sum = 0;
+            for (var i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    var maxHeight = Math.Min(rowMax[i], colMax[j]);
+                    sum += (maxHeight - grid[i][j]);
+                }
+            }
+
+            return sum;
+        }
     }
 }
