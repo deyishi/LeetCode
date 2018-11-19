7a911f7f828799f150f0bcc78e53ae67d19cd9a1
David Shi <dshi@yaharasoftware.com>
2018-11-19 00:11:50 -0600

    11/18/2018 160. Intersection of Two Linked Lists 203. Remove Linked List Elements 206. Reverse Linked List 234. Palindrome Linked List 707. Design Linked List

---
+++ LeetCode/DataModel/MyLinkedList.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.DataModel
+{
+    /// <summary>
+    /// Your MyLinkedList object will be instantiated and called as such:
+    /// MyLinkedList obj = new MyLinkedList();
+    /// int param_1 = obj.Get(index);
+    /// obj.AddAtHead(val);
+    /// obj.AddAtTail(val);
+    /// obj.AddAtIndex(index, val);
+    /// obj.DeleteAtIndex(index);
+    /// </summary>
+    public class MyLinkedList
+    {
+        private ListNode _head;
+        private int _size;
+        /** Initialize your data structure here. */
+        public MyLinkedList()
+        {
+        }
+
+        /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+        public int Get(int index)
+        {
+            if (index + 1 < _size)
+            {
+                return -1;
+            }
+
+            var count = index;
+            var temp = _head;
+            while (count >= 0)
+            {
+                temp = temp.next;
+                count--;
+            }
+
+            return temp.val;
+        }
+
+        /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+        public void AddAtHead(int val)
+        {
+            if (_head == null)
+            {
+                _head = new ListNode(val);
+            }
+            else
+            {
+                var node = new ListNode(val) { next = _head };
+                _head = node;
+            }
+            _size++;
+        }
+
+        /** Append a node of value val to the last element of the linked list. */
+        public void AddAtTail(int val)
+        {
+            if (_head == null)
+            {
+                _head = new ListNode(val);
+            }
+            else
+            {
+                var temp = _head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+
+                temp.next = new ListNode(val);
+            }
+
+            _size++;
+        }
+
+        /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+        public void AddAtIndex(int index, int val)
+        {
+            if (index + 1  < _size)
+            {
+                Console.WriteLine("Given index is greater than the length, the node will not be inserted");
+            }
+            else
+            {
+                var count = index;
+                var temp = _head;
+                while (count > 0)
+                {
+                    temp = temp.next;
+                    count--;
+                }
+
+                temp.next = new ListNode(val);
+                _size++;
+            }
+        }
+
+        /** Delete the index-th node in the linked list, if the index is valid. */
+        public void DeleteAtIndex(int index)
+        {
+            if (index + 1 < _size)
+            {
+                Console.WriteLine("Given index is greater than the length, the node will not be deleted");
+            }
+            else
+            {
+                var count = index;
+                var temp = _head;
+                while (count > 0)
+                {
+                    temp = temp.next;
+                    count--;
+                }
+
+                temp.next = temp.next.next;
+                _size--;
+            }
+
+        }
+    }
+}
--- LeetCode/LinkedListPracticeSet1.cs
+++ LeetCode/LinkedListPracticeSet1.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using LeetCode.DataModel;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 
 namespace LeetCode
 {
@@ -95,5 +96,262 @@ namespace LeetCode
             return false;
         }
 
+
+        [Test]
+        public void GetIntersectionNode()
+        {
+            var a = new[] {1, 2 };
+            var b = new[] {1, 2, 3};
+            var c = new[] {4, 5, 6};
+            var al = CreateLinkedListFromArray(a);
+            var bl = CreateLinkedListFromArray(b);
+            var cl = CreateLinkedListFromArray(c);
+            al.next.next = cl;
+            bl.next.next.next = cl;
+            var r = GetIntersectionNodeTwo(al,bl);
+        }
+
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            if (headA == null || headB == null) return null;
+
+            // Make two list the same size by cutting longer one.
+            var aLength = FindLinkedListLength(headA);
+            var bLength = FindLinkedListLength(headB);
+
+            if (aLength > bLength)
+            {
+                while (aLength != bLength)
+                {
+                    headA = headA.next;
+                    aLength--;
+                }
+            }
+            else
+            {
+                while (aLength != bLength)
+                {
+                    headB = headB.next;
+                    bLength--;
+                }
+            }
+
+            while (headA != headB)
+            {
+                headA = headA.next;
+                headB = headB.next;
+            }
+
+            return headA;
+        }
+
+        public ListNode GetIntersectionNodeTwo(ListNode headA, ListNode headB)
+        {
+            if (headA == null || headB == null) return null;
+            ListNode a = headA, b = headB;
+            while (a != b)
+            {
+
+                if (a != null)
+                {
+                    a = a.next;
+                }
+                else
+                {
+                    a = headB;
+                }
+
+                if (b != null)
+                {
+                    b = b.next;
+                }
+                else
+                {
+                    b = headA;
+                }
+            }
+
+            return a;
+        }
+
+
+ 
+
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            var res = new ListNode(0){next = head};
+            var prevNode = res;
+            while (head != null)
+            {
+                if (head.val != val)
+                {
+                    prevNode = head;
+                }
+                else
+                {
+                    prevNode.next = head.next;
+                }
+
+                head = head.next;
+            }
+
+            return res.next;
+        }
+
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null) return null;
+            ListNode pre = null;
+            ListNode curr = head;
+
+            while (curr != null)
+            {
+                var next = curr.next;
+                curr.next = pre;
+                pre = curr;
+                curr = next;
+            }
+
+            return pre;
+        }
+        public bool IsPalindrome(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return true;
+            }
+
+            var stack = new Stack<int>();
+            stack.Push(head.val);
+            var fast = head;
+            var slow = head;
+
+            // Find mid
+            while (fast.next != null && fast.next.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+                stack.Push(slow.val);
+            }
+
+            // It's odd num remove the mid for checking.
+            if (fast.next == null)
+            {
+                stack.Pop();
+            }
+
+            while (slow.next != null)
+            {
+                slow = slow.next;
+                var temp = stack.Pop();
+                if (temp != slow.val)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        [Test]
+        public void LinkedListTest()
+        {
+            var a = new[] { 1, 2, 3, 3, 2, 1 };
+            var al = CreateLinkedListFromArray(a);
+            var r = IsPalindromeTwo(al);
+        }
+
+        public bool IsPalindromeTwo(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return true;
+            }
+
+            var fast = head;
+            var slow = head;
+
+            // Find mid
+            while (fast.next != null && fast.next.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            // Reverse from mid to end
+            var curr = slow.next;
+            ListNode prev = null;
+            while (curr != null)
+            {
+                var next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+
+            slow.next = prev;
+
+            while (slow.next != null)
+            {
+                slow = slow.next;
+                if (head.val != slow.val)
+                {
+                    return false;
+                }
+
+                head = head.next;
+            }
+
+            return true;
+        }
+
+        public static int FindLinkedListLength(ListNode list)
+        {
+            var i = 0;
+            var curr = list;
+            while (curr != null)
+            {
+                i++;
+                curr = curr.next;
+            }
+
+            return i;
+        }
+
+        public static ListNode CreateLinkedListFromArray(int[] a)
+        {
+            if (a == null || a.Length < 1)
+            {
+                return null;
+            }
+
+            var head = new ListNode(a[0]);
+            var curr = head;
+           
+            for (var i = 1; i < a.Length; i++)
+            {
+                curr.next = new ListNode(a[i]);
+                curr = curr.next;
+            }
+
+            return head;
+        }
+
+        [Test]
+        public void MyLinkedList()
+        {
+            var linkedList = new MyLinkedList();
+            linkedList.AddAtHead(1);
+            linkedList.AddAtTail(3);
+            linkedList.AddAtIndex(1, 2);  // linked list becomes 1->2->3
+            linkedList.Get(1);            // returns 2
+            linkedList.DeleteAtIndex(1);  // now the linked list is 1->3
+            linkedList.Get(1);            // returns 3
+        }
     }
 }
