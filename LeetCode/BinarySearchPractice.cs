91f67f096127f7be4e6c805686b42fa2620aad8b
David Shi <dshi@yaharasoftware.com>
2018-11-25 00:01:35 -0600

    33. Search in Rotated Sorted Array 29. Divide Two Integers 34. Find First and Last Position of Element in Sorted Array 153. Find Minimum in Rotated Sorted Array

---
+++ LeetCode/BinarySearchPractice.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LeetCode
+{
+    public class BinarySearchPractice
+    {
+
+        [Test]
+        public void BinarySearch()
+        {
+            var n = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 12, 12, 12, 12, 12, 15 };
+            var t = 15;
+
+            var l = n.Length - 1;
+            var m = 0;
+            var c = (m - 1 + l) % l;
+            var r = BinarySearch(n, t);
+            var r2 = BinarySearchFindOccurrenceRange(n, t);
+        }
+        public int BinarySearch(int[] nums, int target)
+        {
+            if (nums == null || nums.Length < 1)
+            {
+                return -1;
+            }
+
+            var l = 0;
+            var r = nums.Length - 1;
+            while (l <= r)
+            {
+                var m = l + (r - l) / 2;
+                if (nums[m] == target)
+                {
+                    return m;
+                }
+
+                if (nums[m] > target)
+                {
+                    r = m - 1;
+                }
+                else
+                {
+                    l = m + 1;
+                }
+            }
+
+            return -1;
+        }
+
+        public int BinarySearchRangeStart(int[] nums, int target, bool rangeStart = true)
+        {
+            if (nums == null || nums.Length < 1)
+            {
+                return -1;
+            }
+
+            var l = 0;
+            var r = nums.Length - 1;
+            var result = -1;
+            while (l <= r)
+            {
+                var m = l + (r - l) / 2;
+                if (nums[m] == target)
+                {
+                    result = m;
+                    if (rangeStart)
+                    {
+                        r = m - 1;
+                    }
+                    else
+                    {
+                        l = m + 1;
+                    }
+
+                }
+                else if (nums[m] > target)
+                {
+                    r = m - 1;
+                }
+                else
+                {
+                    l = m + 1;
+                }
+            }
+
+            return result;
+        }
+
+        public int[] BinarySearchFindOccurrenceRange(int[] nums, int target)
+        {
+            var start = BinarySearchRangeStart(nums, target);
+            var end = BinarySearchRangeStart(nums, target, false);
+            return new[] {start, end};
+        }
+
+        [Test]
+        public void FindMin()
+        {
+            var n = new[] {2, 1};
+            var r = FindMin(n);
+        }
+        public int FindMin(int[] nums)
+        {
+            if (nums == null || nums.Length < 1)
+            {
+                return -1;
+            }
+
+            var n = nums.Length;
+            var l = 0;
+            var r = n -1;
+            while (l <= r)
+            {
+                if (nums[l] <= nums[r])
+                {
+                    return l;
+                }
+
+                var m = l + (r - l) / 2;
+
+                var mn = (m + 1) % n;
+                var mp = (m - 1 + n) % n;
+                if (nums[m] <= nums[mn] && nums[m] <= nums[mp])
+                {
+                    return m;
+                }
+
+                if (nums[m] <= nums[r])
+                {
+                    r = m - 1;
+                }
+                else
+                {
+                    l = m + 1;
+                }
+            }
+
+            // Array not sorted.
+            return -1;
+        }
+
+        public int SearchCircularSortedArray(int[] nums, int target)
+        {
+            if (nums == null || nums.Length < 1)
+            {
+                return -1;
+            }
+
+            var l = 0;
+            var r = nums.Length - 1;
+            while (l <= r)
+            {
+                var m = l + (r - l) / 2;
+
+                if (nums[m] == target)
+                {
+                    return m;
+                }
+
+                // check which half is sorted
+                if (nums[m] <= nums[r])
+                {
+                    // Right half is sorted
+                    // update range to be sorted half and look for target
+                    if (nums[m] <= target && target < nums[r])
+                    {
+                        l = m + 1;
+                    }
+                    else
+                    {
+                        r = m - 1;
+                        // update range to the unsorted half
+                    }
+                }
+                else
+                {
+                    if (nums[l] <= target && target < nums[m])
+                    {
+                        r = m - 1;
+                    }
+                    else
+                    {
+                        l = m + 1;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LeetCode/PracticeSet3.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LeetCode
+{
+    public class PracticeSet3
+    {
+
+        [Test]
+        public void Divide()
+        {
+
+            var r = DivideTwo(-2147483648, -1);
+        }
+
+        public int Divide(int dividend, int divisor)
+        {
+
+            // Sign
+            var negative = (dividend < 0) ^ (divisor < 0);
+
+            //Limit
+            long lDividend = Math.Abs((long) dividend);
+            long lDivisor = Math.Abs((long) divisor);
+
+
+            // case 0 
+            if (dividend == 0 || lDividend < lDivisor)
+            {
+                return 0;
+            }
+
+            var result = DivideHelper(lDividend, lDivisor);
+
+            if (result > int.MaxValue)
+            {
+                return negative ? int.MinValue : int.MaxValue;
+            }
+
+            return negative ? (int) -result : (int) result;
+
+        }
+
+        public int DivideTwo(int dividend, int divisor)
+        {
+            // Sign
+            var negative = (dividend < 0) ^ (divisor < 0);
+
+            //Limit
+            long lDividend = Math.Abs((long)dividend);
+            long lDivisor = Math.Abs((long)divisor);
+
+            // case 0 
+            if (lDividend == 0 || lDividend < lDivisor)
+            {
+                return 0;
+            }
+
+            long result = 0;
+            while (lDividend >= lDivisor)
+            {
+                long sum = lDivisor;
+                long multiple = 1;
+                while (sum << 1 <= lDividend)
+                {
+                    sum  <<= 1;
+                    multiple <<= 1;
+                }
+                lDividend -= sum;
+                result += multiple;
+            }
+
+            if (result > int.MaxValue)
+            {
+                return negative ? int.MinValue : int.MaxValue;
+            }
+
+            return negative ? (int)-result : (int)result;
+        }
+
+        private long DivideHelper(long lDividend, long lDivisor)
+        {
+            if (lDividend < lDivisor)
+            {
+                return 0;
+            }
+
+            var sum = lDivisor;
+            long multiple = 1;
+
+            while (sum << 1 <= lDividend)
+            {
+                sum = sum << 1;
+                multiple += multiple;
+            }
+
+            return multiple + DivideHelper(lDividend - sum, lDivisor);
+        }
+
+        [Test]
+        public void Search()
+        {
+            var n = new[] {3, 1};
+            var r = Search(n, 1);
+        }
+        public int Search(int[] nums, int target)
+        {
+            if (nums == null || nums.Length < 1)
+            {
+                return -1;
+            }
+
+            var l = 0;
+            var r = nums.Length - 1;
+
+            while (l <= r)
+            {
+
+                var m = (l + r) / 2;
+                if (target == nums[m]) return m;
+                if (nums[m] >= nums[l])
+                {
+                    if (target >= nums[l] && target <= nums[m])
+                    {
+                        r = m - 1;
+                    }
+                    else
+                    {
+                        l = m + 1;
+                    }
+                }
+                else
+                {
+                    if (target >= nums[m] && target <= nums[r])
+                    {
+                        l = m + 1;
+                    }
+                    else
+                    {
+                        r = m - 1;
+                    }
+                }
+            }
+
+
+            return -1;
+        }
+
+        public int[] SearchRange(int[] nums, int target)
+        {
+            return null;
+        }
+    }
+}
