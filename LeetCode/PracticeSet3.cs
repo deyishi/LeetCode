d02386d56ff97093e0083bc6d8d8d65f2c7259b6
David Shi <dshi@yaharasoftware.com>
2018-11-25 22:22:44 -0600

    30. Substring with Concatenation of All Words 155. Min Stack 157. Read N Characters Given Read4 168. Excel Sheet Column Title 170. Two Sum III - Data structure design 172. Factorial Trailing Zeroes

--- LeetCode/PracticeSet3.cs
+++ LeetCode/PracticeSet3.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -150,9 +151,108 @@ namespace LeetCode
             return -1;
         }
 
-        public int[] SearchRange(int[] nums, int target)
+
+
+        public int Read(char[] buf, int n)
         {
-            return null;
+            int index = 0;
+            char[] r4 = new char[4];
+            while (index < n)
+            {
+                // Place holder
+                //int c = read4(r4);
+                var c = 4;
+                for (int i = 0; i < c && index < n; i++)
+                {
+                    buf[index++] = r4[i];
+                }
+                if (c < 4) break;
+            }
+
+            return index;
+        }
+
+        [Test]
+        public void ConvertToTitle()
+        {
+            var n = 28;
+            var r = ConvertToTitle(n);
+        }
+
+        public string ConvertToTitle(int n)
+        {
+            var result = "";
+            if (n == 0)
+            {
+                return result;
+            }
+
+            while (n > 0)
+            {
+                // Start at A so we need n--;
+                n--;
+                result = (char) ('A' + n % 26) + result;
+                n /= 26;
+            }
+
+            return result;
+        }
+
+        public string ConvertToTitleTwo(int n)
+        {
+            return n == 0 ? "" : ConvertToTitle(--n / 26) + (char)('A' + (n % 26));
+        }
+
+        [Test]
+        public void TwoSum()
+        {
+            var t = new TwoSum();
+
+            t.Add(3);
+            t.Add(5);
+
+            var r = t.Find(6);
+        }
+
+    }
+
+    public class TwoSum
+    {
+
+        private Dictionary<int, int> dict;
+        /** Initialize your data structure here. */
+        public TwoSum()
+        {
+            dict = new Dictionary<int, int>();
+        }
+
+        /** Add the number to an internal data structure.. */
+        public void Add(int number)
+        {
+            if (dict.ContainsKey(number))
+            {
+                dict[number]++;
+            }
+            else
+            {
+                dict.Add(number, 1);
+            }
+        }
+
+        /** Find if there exists any pair of numbers which sum is equal to the value. */
+        public bool Find(int value)
+        {
+            foreach (var num in dict.Keys)
+            {
+                var t = value - num;
+
+                if (t != num && dict.ContainsKey(t) ||  t == num  && dict[t] > 1)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
