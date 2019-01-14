39ca8a0eed2920d49248200f63fdb57cab401475
David Shi <dshi@yaharasoftware.com>
2019-01-13 22:02:03 -0600

    76. Minimum Window Substring Two char[] array, one for current string char (start empty), one for target string char (scan target string). Two counters C and K, one for unique char from target string found in current substring (when current string char map[char] == targetmap[char], increase count), one for unique char found in target string (ex. Target ABBC, unique 3). Loop through string and increment or decrement two counter and left and right point for result substring index. At each i, if  two counters are equal, there is a window found for this i and record that to result.

--- LeetCode/TwoPointersSet.cs
+++ LeetCode/TwoPointersSet.cs
@@ -52,5 +52,85 @@ namespace LeetCode
 
             return result;
         }
+
+
+        [Test]
+        public void MinWindow()
+        {
+            var s = "ADOBECODEBANC";
+            var t = "ABC";
+
+            var r = MinWindow(s, t);
+        }
+
+
+        /// <summary>
+        /// Two char[] array, one for current string char (start empty), one for target string char (scan target string).
+        /// Two counters C and K, one for unique char from target string found in current substring (when current string char map[char] == targetmap[char], increase count), one for unique char found in target string (ex. Target ABBC, unique 3).
+        /// Loop through string and increment or decrement two counter and left and right point for result substring index.
+        /// At each i, if  two counters are equal, there is a window found for this i and record that to result.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public string MinWindow(string s, string t)
+        {
+            if (string.IsNullOrEmpty(t))
+            {
+                return "";
+            }
+
+            var map = new int[256];
+            var targetMap = new int[256];
+            var found = 0;
+            var target = 0;
+            foreach (var c in t)
+            {
+                targetMap[c]++;
+                if (targetMap[c] == 1)
+                {
+                    target++;
+                }
+            }
+
+            int ansStartIndex = -1, ansEndIndex = -1;
+
+            int r = 0;
+
+            for (var l =0; l < s.Length;l++)
+            {
+                while (r < s.Length && found < target)
+                {
+                    map[s[r]]++;
+                    if (map[s[r]] == targetMap[s[r]])
+                    {
+                        found++;
+                    }
+                    r++;
+                }
+
+                if (found == target)
+                {
+                    if (ansStartIndex == -1 || r-l < ansEndIndex-ansStartIndex)
+                    {
+                        ansEndIndex = r;
+                        ansStartIndex = l;
+                    }
+                }
+
+                map[s[l]]--;
+                if (map[s[l]] == targetMap[s[l]] -1)
+                {
+                    found--;
+                }
+            }
+
+            if (ansStartIndex == -1)
+            {
+                return "";
+            }
+
+            return s.Substring(ansStartIndex, ansEndIndex-ansStartIndex);
+        }
     }
 }
