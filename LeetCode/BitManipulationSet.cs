1da80d699d1eaa403179cb44a7cb162803f9f558
David Shi <dshi@yaharasoftware.com>
2019-01-14 16:09:46 -0600

    147. Insertion Sort List Create a new list. Loop through linked list, find the position for the current node in the new list (loop through new list find a node where val is >= current node val), then insert current node.

--- LeetCode/BitManipulationSet.cs
+++ LeetCode/BitManipulationSet.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LeetCode.DataModel;
 using NUnit.Framework;
 
 namespace LeetCode
@@ -44,6 +45,48 @@ namespace LeetCode
             }
 
             return ones;
+        }
+
+
+        [Test]
+        public void InsertionSortList()
+        {
+            var h = new[] {3, 2, 5, 1}.ToLinkedList();
+            var r = InsertionSortList(h);
+        }
+
+        /// <summary>
+        /// Create a new list.
+        /// Loop through linked list, find the position for the current node in the new list (loop through new list find a node where val is >= current node val), then insert current node. 
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public ListNode InsertionSortList(ListNode head)
+        {
+            var dummy = new ListNode(0);
+
+            while (head != null)
+            {
+                var node = dummy;
+
+                // move pointer in new list to add new node if its val is smaller than current dummy[0].
+                while (node.next != null && node.next.val < head.val)
+                {
+                    node = node.next;
+                }
+
+                //Save next step
+                var temp = head.next;
+
+                //Insert current node into new list. Node is at place where current node should be inserted, so head.next = node.next, then node.next = head. 
+                head.next = node.next;
+                node.next = head;
+
+                //Move to next step
+                head = temp;
+            }
+
+            return dummy.next;
         }
     }
 }
