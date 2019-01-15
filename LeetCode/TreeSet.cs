5c6533656129edf8ed34001e9fdbc93d7d529964
David Shi <dshi@yaharasoftware.com>
2019-01-15 14:42:42 -0600

    156. Binary Tree Upside Down Use dfs update all the left most nodes. parent.left(root node).right = parent parent.left(root node).left = parent.right Cut leaf node converted from parent parent.left == null parent.right == null

---
+++ LeetCode/TreeSet.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.DataModel;
+using NUnit.Framework;
+
+namespace LeetCode
+{
+    public class TreeSet
+    {
+        [Test]
+        public void PreorderTraversal()
+        {
+            var t = new int[] { 1, 2, 3, 4, 5 }.ToTree();
+
+            var r = UpsideDownBinaryTree(t);
+        }
+
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            var result = new List<int>();
+            if (root == null)
+            {
+                return result;
+            }
+
+            PreorderTraversalHelper(result, root);
+            return result;
+        }
+
+        private void PreorderTraversalHelper(List<int> result, TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            result.Add(node.val);
+            PreorderTraversalHelper(result, node.left);
+            PreorderTraversalHelper(result, node.right);
+        }
+
+        /// <summary>
+        /// Use dfs update all the left most nodes.
+        /// parent.left(root node).right = parent
+        /// parent.left(root node).left = parent.right
+        /// Cut leaf node converted from parent
+        /// parent.left == null
+        /// parent.right == null
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public TreeNode UpsideDownBinaryTree(TreeNode root)
+        {
+            if (root == null || root.left == null)
+            {
+                return root;
+            }
+
+            TreeNode node = UpsideDownBinaryTree(root.left);
+            root.left.right = root;
+            root.left.left = root.right;
+            root.left = null;            // important
+            root.right = null;
+            return node;
+        }
+    }
+}
