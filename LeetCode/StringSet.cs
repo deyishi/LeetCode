a444da81f1eebdc4896d876c68a39aa1a4dd7fdb
David Shi <dshi@yaharasoftware.com>
2019-01-15 11:02:49 -0600

    151. Reverse Words in a String  Split string by space.  Loop from the last word and append to string builder.  Condition check if word is empty then append space if word isn't the first in string builder.

--- LeetCode/StringSet.cs
+++ LeetCode/StringSet.cs
@@ -25,7 +25,7 @@ namespace LeetCode
 
             //Result, Stack to track position of '(', CurrentSetLength(Length of current finished parentheses using all the '(' in the Stack).
             var result = 0;
-            var currentSetCount = 0; 
+            var currentSetCount = 0;
             var locationStack = new Stack<int>();
 
             //Loop through parentheses string
@@ -64,6 +64,49 @@ namespace LeetCode
             }
 
             return result;
+        }
+
+
+        [Test]
+        public void ReverseWords()
+        {
+            var s = "  1";
+            var r = ReverseWords(s);
+        }
+
+        /// <summary>
+        /// Split string by space.
+        /// Loop from the last word and append to string builder.
+        /// Condition check if word is empty then append space if word isn't the first in string builder.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public string ReverseWords(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return "";
+            }
+
+            var a = s.Split(' ');
+            var sb = new StringBuilder();
+            for (var i = a.Length - 1; i >= 0; i--)
+            {
+                // Handle empty string before space.
+                if (!a[i].Equals(""))
+                {
+                    //If not first word, append space.
+                    if (sb.Length > 0)
+                    {
+                        sb.Append(" ");
+                    }
+
+                    // Append word.
+                    sb.Append(a[i]);
+                }
+            }
+
+            return sb.ToString();
         }
     }
 }
