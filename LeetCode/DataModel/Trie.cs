64a85c2f51245409bade171698e62d6fa96a903a
David Shi <dshi@yaharasoftware.com>
2019-01-20 23:18:03 -0600

    208. Implement Trie (Prefix Tree) Map to store char and TrieNode IsWordEnd.

---
+++ LeetCode/DataModel/Trie.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.DataModel
+{
+    public class Trie
+    {
+        private class TrieNode
+        {
+            public Dictionary<char, TrieNode> Children;
+            public bool IsWordEnd;
+            public TrieNode()
+            {
+                Children = new Dictionary<char, TrieNode>();
+            }
+        }
+
+        private readonly TrieNode _root;
+        public Trie()
+        {
+            _root = new TrieNode();
+        }
+
+        /** Inserts a word into the trie. */
+        public void Insert(string word)
+        {
+            TrieNode current = _root;
+            foreach (var c in word.ToCharArray())
+            {
+                if (!current.Children.ContainsKey(c))
+                {
+                    var node = new TrieNode();
+                    current.Children.Add(c, node);
+                }
+
+                current = current.Children[c];
+            }
+
+            current.IsWordEnd = true;
+        }
+
+        /** Returns if the word is in the trie. */
+        public bool Search(string word)
+        {
+            TrieNode current = _root;
+            foreach (var c in word.ToCharArray())
+            {
+                if (!current.Children.ContainsKey(c))
+                {
+                    return false;
+                }
+
+                current = current.Children[c];
+            }
+
+            return current.IsWordEnd;
+        }
+
+        /** Returns if there is any word in the trie that starts with the given prefix. */
+        public bool StartsWith(string prefix)
+        {
+            TrieNode current = _root;
+            foreach (var c in prefix.ToCharArray())
+            {
+                if (!current.Children.ContainsKey(c))
+                {
+                    return false;
+                }
+
+                current = current.Children[c];
+            }
+
+            return true;
+        }
+    }
+}
