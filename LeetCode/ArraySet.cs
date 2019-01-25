9954032a5821492821ac6e764538d600058f3b97
David Shi <dshi@yaharasoftware.com>
2019-01-25 00:54:36 -0600

    238. Product of Array Except Self Product from the left, left[i] = nums[0] * .... * nums[i-1]  * nums[i] Product from the right, right[i] = nums[i] * nums[i+1]  * .... * nums[len(nums)] Calculate 4 for [1,2,3,4,5,6,7] 4 = left[3](1*2*3) * right[3] (7*6*5)

--- LeetCode/ArraySet.cs
+++ LeetCode/ArraySet.cs
@@ -13,10 +13,10 @@ namespace LeetCode
         [Test]
         public void Test()
         {
-            var n = new[] { 1,1,1,2,2,2,3 };
+            var n = new[] {1,2,3,4 };
             var s = 0;
             var e = 99;
-            var r = MajorityElement(n);
+            var r = ProductExceptSelfTwo(n);
         }
 
 
@@ -299,6 +299,32 @@ namespace LeetCode
             if (count2 > mark)
             {
                 result.Add(candidate2);
+            }
+
+            return result;
+        }
+
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            //238.Product of Array Except Self
+            //Product from the left, left[i] = nums[0] * .... * nums[i - 1] * nums[i]
+            //Product from the right, right[i] = nums[i] * nums[i + 1] * .... * nums[len(nums)]
+            //Calculate 4 for [1, 2, 3, 4, 5, 6, 7]
+            //4 = left[3](1 * 2 * 3) * right[3](7 * 6 * 5)
+            int n = nums.Length;
+            int[] result = new int[n];
+
+            result[0] = 1;
+            for (int i = 1; i < n; i++)
+            {
+                result[i] = result[i - 1] * nums[i - 1];
+            }
+
+            int right = 1;
+            for (int i = n-1; i >= 0; i--)
+            {
+                result[i] *= right;
+                right *= nums[i];
             }
 
             return result;
