c81f0614c6d806eadf97af3b759c8c100459e280
David Shi <dshi@yaharasoftware.com>
2019-02-01 23:13:37 -0600

    303. Range Sum Query - Immutable Find sum to the point i and put it in nums[i]. Given j, i, nums[j] - nums[i-1] is the sum from i to j.

---
+++ LeetCode/DataModel/NumArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.DataModel
+{
+    public class NumArray
+    {
+        private readonly int[] _nums;
+        public NumArray(int[] nums)
+        {
+            _nums = nums;
+            // Count total at num[i]
+            for (int i = 1; i < _nums.Length; i++)
+            {
+                _nums[i] = _nums[i] + _nums[i - 1];
+            }
+        }
+
+        public int SumRange(int i, int j)
+        {
+            if (i == 0)
+            {
+                return _nums[j];
+            }
+
+            return _nums[j] - _nums[i - 1];
+        }
+    }
+}
