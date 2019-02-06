8338b77ed80d85d5f33cc05fe11f65d1a5e9e47a
David Shi <dshi@yaharasoftware.com>
2019-02-06 11:05:25 -0600

    288. Unique Word Abbreviation Use a dictionary and hash set to keep track all the words belong to the same abbreviation.

---
+++ LeetCode/DataModel/ValidWordAbbr.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LeetCode.DataModel
+{
+    public class ValidWordAbbr
+    {
+
+        private Dictionary<string, HashSet<string>> _map = new Dictionary<string, HashSet<string>>();
+        public ValidWordAbbr(string[] dictionary)
+        {
+            foreach (var word in dictionary)
+            {
+                var abbr = GetWordAbbr(word);
+                if (_map.ContainsKey(abbr))
+                {
+                  _map[abbr].Add(word);
+                }
+                else
+                {
+                    _map.Add(abbr, new HashSet<string> {word});
+                }
+
+
+            }
+        }
+
+        public bool IsUnique(string word)
+        {
+            var abbr = GetWordAbbr(word);
+
+            return !_map.ContainsKey(abbr) || _map[abbr].Count == 1 && _map[abbr].Contains(word);
+        }
+
+        public string GetWordAbbr(string word)
+        {
+            if (word == null || word.Length < 3)
+            {
+                return word;
+            }
+
+            return word[0] + (word.Length - 2).ToString() + word[word.Length - 1];
+        }
+    }
+}
