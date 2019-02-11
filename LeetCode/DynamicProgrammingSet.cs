6d8c2757621c8c9241e8ff40bbca2875bdd8f385
David Shi <dshi@yaharasoftware.com>
2019-02-10 23:54:38 -0600

    97. Interleaving String dp[s1.Length + 1][s2.Length +1] dp[i,j] = s1[i-1] == s3[i+j-1] and dp[i-1,j] or s2[j-1] == s3[i+j-1] and dp[i,j-1]

--- LeetCode/DynamicProgrammingSet.cs
+++ LeetCode/DynamicProgrammingSet.cs
@@ -13,14 +13,10 @@ namespace LeetCode
         [Test]
         public void Test()
         {
-            var a = new char[,]
-            {
-                {'1', '0', '1', '0', '0'}, {'1', '0', '1', '1', '1'}, {'1', '1', '1', '1', '1'},
-                {'1', '0', '0', '1', '0'}
-            };
-
-            var t = new int[] {3, 3, 5, 0, 0, 3, 1, 4};
-            var r = StockThree(t);
+            var a = "aabcc";
+            var b = "dbbca";
+            var c = "aadbbcbcac";
+            var r = IsInterleave(a, b, c);
         }
 
         /// <summary>
@@ -739,7 +735,39 @@ namespace LeetCode
             }
 
             return sell[day - 1];
+        }
 
+
+        public bool IsInterleave(string s1, string s2, string s3)
+        {
+            if (s1.Length + s2.Length != s3.Length)
+            {
+                return false;
+            }
+
+            var dp = new bool[s1.Length + 1, s2.Length + 1];
+            dp[0, 0] = true;
+            for (var i = 1; i <= s2.Length; i++)
+            {
+                dp[0, i] = s2[i - 1] == s3[i - 1] && dp[0, i - 1];
+            }
+
+            for (var i = 1; i <= s1.Length; i++)
+            {
+                dp[i, 0] = s1[i - 1] == s3[i - 1] && dp[i - 1, 0];
+            }
+
+            for (var i = 1; i <= s1.Length; i++)
+            {
+                for (var j = 1; j <= s2.Length; j++)
+                {
+
+                    dp[i, j] = dp[i - 1, j] && s1[i - 1] == s3[i + j - 1] ||
+                               dp[i, j - 1] && s2[j - 1] == s3[i + j - 1];
+                }
+            }
+
+            return dp[s1.Length, s2.Length];
         }
     }
 }
