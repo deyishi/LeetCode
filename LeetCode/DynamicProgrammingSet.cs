178bc598aaea510ec00d452747fee09892ad1d37
David Shi <dshi@yaharasoftware.com>
2019-02-11 00:16:51 -0600

    115. Distinct Subsequences

--- LeetCode/DynamicProgrammingSet.cs
+++ LeetCode/DynamicProgrammingSet.cs
@@ -13,10 +13,9 @@ namespace LeetCode
         [Test]
         public void Test()
         {
-            var a = "aabcc";
-            var b = "dbbca";
-            var c = "aadbbcbcac";
-            var r = IsInterleave(a, b, c);
+            var s = "rabbbit";
+            var t = "rabbit";
+            var r = NumDistinct(s, t);
         }
 
         /// <summary>
@@ -768,6 +767,33 @@ namespace LeetCode
             }
 
             return dp[s1.Length, s2.Length];
+        }
+
+        public int NumDistinct(string s, string t)
+        {
+            var dp = new int[t.Length + 1, s.Length + 1];
+
+            for (int i = 0; i <= s.Length; i++)
+            {
+                dp[0, i] = 1;
+            }
+
+            for (int i = 1; i <= t.Length; i++)
+            {
+                for (int j = 1; j <= s.Length; j++)
+                {
+                    if (t[i-1] == s[j-1])
+                    {
+                        dp[i, j] = dp[i-1, j-1] + dp[i, j-1];
+                    }
+                    else
+                    {
+                        dp[i, j] = dp[i, j - 1];
+                    }
+                }
+            }
+
+            return dp[t.Length, s.Length];
         }
     }
 }
