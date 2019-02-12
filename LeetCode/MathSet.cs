126c82f3b0af12c6e7a85802ae2fd68e99eb643d
David Shi <dshi@yaharasoftware.com>
2019-02-11 23:40:27 -0600

    319. Bulb Switcher

--- LeetCode/MathSet.cs
+++ LeetCode/MathSet.cs
@@ -511,5 +511,22 @@ namespace LeetCode
 
             return dp[n - 1];
         }
+
+        public int BulbSwitch(int n)
+        {
+            return (int)Math.Sqrt(n);
+        }
+
+        public int FlipLights(int n, int m)
+        {
+            if (m == 0) return 1;
+            if (n == 1) return 2;
+            if (n == 2 && m == 1) return 3;
+            if (n == 2) return 4;
+            if (m == 1) return 4;
+            if (m == 2) return 7;
+            if (m >= 3) return 8;
+            return 8;
+        }
     }
 }
