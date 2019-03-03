88a6560bfdf318676df05dbc7ca284d6aed31118
David Shi <dshi@yaharasoftware.com>
2019-03-03 13:11:13 -0600

    359. Logger Rate Limiter Dictionary to track message timestamp. timestamp < map[message]

---
+++ LeetCode/Design/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Design
+{
+    public class Logger
+    {
+        private Dictionary<string, int> _messageQueue;
+
+        public Logger()
+        {
+            _messageQueue = new Dictionary<string, int>();
+        }
+
+        public bool ShouldPrintMessage(int timestamp, string message)
+        {
+            // Check if its a new message, else increase its time span.
+            if (!_messageQueue.ContainsKey(message))
+            {
+                _messageQueue.Add(message, 0);
+            }
+
+            if ( timestamp < _messageQueue[message])
+            {
+                return false;
+            }
+
+            _messageQueue[message] = timestamp + 10;
+            return true;
+        }
+    }
+}
