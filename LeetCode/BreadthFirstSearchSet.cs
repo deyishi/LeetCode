56ceccaf589fef6c45f67f19738be91652e9452b
David Shi <dshi@yaharasoftware.com>
2019-03-04 21:42:39 -0600

    351. Android Unlock Patterns int[,] to track which two points need a point to pass. visited[] to track if point has been visited (mark and unmark during backtracking) Loop through m to n, do dfs on each points count. DFS returns on remain is  <= 0. In dfs loop through 1-9, check if a point is visited and if  curr to this point require a point to be visited.

--- LeetCode/BreadthFirstSearchSet.cs
+++ LeetCode/BreadthFirstSearchSet.cs
@@ -380,5 +380,60 @@ namespace LeetCode
                 }
             }
         }
+
+        public int NumberOfPatterns(int m, int n)
+        {
+            // Number required to pass two point.
+            int[,] skip = new int[10, 10];
+            skip[1, 3] = skip[3, 1] = 2;
+            skip[7, 1] = skip[1, 7] = 4;
+            skip[3, 9] = skip[9, 3] = 6;
+            skip[7, 9] = skip[9, 7] = 8;
+            skip[1, 9] = skip[2, 8] = skip[3, 7] = skip[6, 4] = skip[9, 1] = skip[8, 2] = skip[7, 3] = skip[4, 6] = 5;
+
+
+            int result = 0;
+            bool[] visited = new bool[10];
+            for (int i = m; i <= n; i++)
+            {
+                // 1,3,7,9 share the same path.
+                // 2,4,6,8 share the same path.
+                // 5 
+
+                // Do dfs on every points count.
+                result += NumberOfPatternsHelper(skip, visited, 1, i - 1) * 4;
+                result += NumberOfPatternsHelper(skip, visited, 2, i - 1) * 4;
+                result += NumberOfPatternsHelper(skip, visited, 5, i - 1);
+            }
+
+            return result;
+        }
+
+        private int NumberOfPatternsHelper(int[,] skip, bool[] visited, int curr, int remain)
+        {
+            if (remain < 0)
+            {
+                return 0;
+            }
+
+            if (remain == 0)
+            {
+                return 1;
+            }
+
+            visited[curr] = true;
+
+            int result = 0;
+            for (var i = 1; i <= 9; i++)
+            {
+                if (!visited[i] && (skip[curr, i] == 0 || visited[skip[curr, i]]))
+                {
+                    result += NumberOfPatternsHelper(skip, visited, i, remain - 1);
+                }
+            }
+
+            visited[curr] = false;
+            return result;
+        }
     }
 }
